ec28ca9ba2dfdb04716d15c9d69d1f6fd0bbf9e4
Maxim <malex81@yandex.ru>
2026-01-28 14:02:38 +0300

    work around async load vue components

--- Digger/Dwarf.Digger/wwwroot/mjs/base/vue-helper.mjs
+++ Digger/Dwarf.Digger/wwwroot/mjs/base/vue-helper.mjs
@@ -1,10 +1,19 @@
-﻿const templateLoader = {
+﻿import { defineAsyncComponent } from 'vue'
+
+const templateLoader = {
 	getUrl(templateName) { return `/templates/${templateName}`; },
 	async load(templateUrl) {
 		const resp = await fetch(templateUrl);
 		if (!resp.ok)
 			throw new Error(`An error on load template '${templateUrl}'`);
 		return await resp.text();
+	},
+	buildComponent(templateName, build) {
+		const self = this;
+		return defineAsyncComponent(async () => {
+			const templ = await self.load(self.getUrl(templateName));
+			return build(templ);
+		})
 	}
 }
 
--- Digger/Dwarf.Digger/wwwroot/mjs/vue-blocks/indicator.mjs
+++ Digger/Dwarf.Digger/wwwroot/mjs/vue-blocks/indicator.mjs
@@ -1,18 +1,15 @@
-﻿import { defineAsyncComponent, ref, reactive } from 'vue'
-import { templateLoader as tl} from 'mbase/vue-helper.mjs'
+﻿import { ref, reactive } from 'vue'
+import { templateLoader as ttLoader } from 'mbase/vue-helper.mjs'
 
-export default defineAsyncComponent(async () => {
-	const templ = await tl.load(tl.getUrl("indicator.html"));
-	return {
-		template: templ,
-		setup() {
-			return {
-				st: "1232"
-			}
-		},
+export default ttLoader.buildComponent("indicator.html", templ => ({
+	template: templ,
+	setup() {
+		return {
+			st: "1232-eee"
+		}
+	},
 	//	components: {
 	//		fsNavigator,
 	//		fsViewer,
 	//	}
-	}
-})
+}));
